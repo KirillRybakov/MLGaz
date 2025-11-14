1f7959b06b3809b3f26c4c559b4feff2f4571373
Kirill Rybakov <rybakovkirill82@gmail.com>
2025-11-14 14:28:16 +0300

    fix: calendar removed

--- alfacreator-backend/app/main.py
+++ alfacreator-backend/app/main.py
@@ -4,7 +4,7 @@ from loguru import logger
 import sys
 
 # Импортируем все наши роутеры
-from app.routers import promo, analytics, documents, smart_analytics, history, calendar
+from app.routers import promo, analytics, documents, smart_analytics, history
 # Импортируем все для работы с БД
 from app.database import engine, Base
 from app import models
@@ -46,4 +46,3 @@ app.include_router(analytics.router, prefix="/api/v1/analytics", tags=["Анал
 app.include_router(documents.router, prefix="/api/v1/documents", tags=["Документы"])
 app.include_router(smart_analytics.router, prefix="/api/v1/smart_analytics", tags=["Умная Аналитика"])
 app.include_router(history.router, prefix="/api/v1/history", tags=["История"])
-app.include_router(calendar.router, prefix="/api/v1/calendar", tags=["Умный календарь"])
