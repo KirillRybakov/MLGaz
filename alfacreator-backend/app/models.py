e3e45c7fca925afebb11a7a11eec12777da4439a
Павел Александрович Бережной <bpa0607@yandex.ru>
2025-11-17 10:22:34 +0300

    Update models.py

--- alfacreator-backend/app/models.py
+++ alfacreator-backend/app/models.py
@@ -1,4 +1,3 @@
-# alfacreator-backend/app/models.py
 from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
