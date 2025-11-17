ced043d032a18b52ae591a317fc1819e21327acf
Павел Александрович Бережной <bpa0607@yandex.ru>
2025-11-17 10:07:42 +0300

    Update dependencies.py

--- alfacreator-backend/app/core/dependencies.py
+++ alfacreator-backend/app/core/dependencies.py
@@ -11,11 +11,11 @@ from app.core.config import settings
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
 
 async def get_db():
-    # Используем 'async with' для автоматического и корректного закрытия сессии
+
     async with AsyncSessionLocal() as session:
         yield session
 
-# Указываем правильный тип для db: AsyncSession
+
 async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -31,7 +31,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession
     except JWTError:
         raise credentials_exception
     
-    # Добавляем await для вызова асинхронной функции
+
     user = await crud.get_user_by_email(db, email=token_data.email)
     
     if user is None:
